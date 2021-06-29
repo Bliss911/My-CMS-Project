5d7ed42fa0ff7fa72478a26a2f1467ed3f78939c
Bliss911 <innociousbliss@gmail.com>
2021-06-29 23:45:22 +0100

    Fixed the redirect URI

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class Config(object):
                                                        
     CLIENT_ID = "558163a3-4d46-418a-8035-2141c68d9a73"
                                                      
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "https://localhost:5555/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
                                                      
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
